b6a66a107a776a9f435b78a3e561258648b860d8
Minjun-shin <minjun.shin1201@gmail.com>
2024-01-21 13:17:23 +0900

    Add 11000 Problem

---
+++ gold/11000/11000-1.py
@@ -0,0 +1,20 @@
+import heapq
+
+
+N = int(input())
+classes = [tuple(map(int, input().split())) for _ in range(N)]
+
+classes.sort()
+res = []
+
+for s, t in classes:
+    if not res:
+        res.append(t)
+        continue
+
+    if s >= res[0]:
+        heapq.heappop(res)
+
+    heapq.heappush(res, t)
+
+print(len(res))
---
+++ gold/11000/11000-2.py
@@ -0,0 +1,10 @@
+N = int(input())
+classes = [tuple(map(int, input().split())) for _ in range(N)]
+
+classes.sort()
+res = []
+
+for s, t in classes:
+    pass
+
+print(len(res))
