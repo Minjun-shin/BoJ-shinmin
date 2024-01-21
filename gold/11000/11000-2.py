9559fecf797044cb24e7d761a2318cde0c4c8d84
Minjun-shin <minjun.shin1201@gmail.com>
2024-01-21 13:44:23 +0900

    Update 11000 Problem

--- gold/11000/11000-2.py
+++ gold/11000/11000-2.py
@@ -1,3 +1,8 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+
 N = int(input())
 classes = [tuple(map(int, input().split())) for _ in range(N)]
 
@@ -5,6 +10,13 @@ classes.sort()
 res = []
 
 for s, t in classes:
-    pass
+    if not res:
+        res.append(t)
+        continue
+
+    if s >= res[0]:
+        heapq.heappop(res)
+
+    heapq.heappush(res, t)
 
 print(len(res))
