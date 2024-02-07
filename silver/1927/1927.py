346f7db58c4889d6eb9a019715bb137175e68796
Minjun-shin <minjun.shin1201@gmail.com>
2024-02-07 16:47:21 +0900

    Add 1927 Problem

---
+++ silver/1927/1927.py
@@ -0,0 +1,19 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    command = int(input())
+    if command == 0:
+        if heap:
+            print(heapq.heappop(heap))
+
+        else:
+            print(0)
+
+    else:
+        heapq.heappush(heap, command)
