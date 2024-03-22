355eec84363792b310fbabaf4eb13119c536226b
Minjun-shin <minjun.shin1201@gmail.com>
2024-03-22 11:21:50 +0900

    Add 1629 Problem

---
+++ silver/1629/1629.py
@@ -0,0 +1,12 @@
+def calc(a, b, c):
+    if b == 1:
+        return a % c
+    
+    if b % 2 == 0:
+        return (calc(a, b // 2, c) ** 2) % c
+    else:
+        return (calc(a, b // 2, c) ** 2 * calc(a, 1, c)) % c
+
+
+A, B, C = map(int, input().split())
+print(calc(A, B, C))
